b01f5828a0e7316b0979577282a8c612f38c6a5a
Pavel Kuznetsov <Technic47@gmail.com>
2022-07-29 14:52:51 +0300

    Tasks34, 36, 38 done

---
+++ Task36/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Задача 36: Задайте одномерный массив, 
+заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+*/
+
+Console.WriteLine("Enter array length: ");
+int N = int.Parse(Console.ReadLine()!);
+int[] array = new int[N];
+
+int sum = 0;
+
+for (int i = 0; i < N; i++)
+{
+    array[i] = new Random().Next(0, 10);
+    Console.Write($"{array[i]} ");
+    if ((i % 2) == 0) sum = sum + array[i];
+}
+
+Console.WriteLine($"\nSum of numbers with uneven index: {sum}");
---
+++ Task38/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Задача 38: Задайте массив вещественных чисел. 
+Найдите разницу между максимальным и минимальным элементов массива.
+*/
+
+Console.WriteLine("Enter array length: ");
+int N = int.Parse(Console.ReadLine()!);
+double[] array = new double[N];
+
+double max = 0;
+double min = 99999;
+
+Console.WriteLine("Enter number: ");
+for (int i = 0; i < N; i++)
+{
+    array[i] = double.Parse(Console.ReadLine()!);
+    if (array[i] > max) max = array[i];
+    if (array[i] < min) min = array[i];
+}
+
+Console.Write($"Your array: ");
+for (int i = 0; i < N; i++)
+{
+    Console.Write($"{array[i]}; ");
+}
+
+Console.WriteLine($"\nDifferense between max and min: {max - min}");
